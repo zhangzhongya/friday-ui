90fc67fd792101928cfd0f057eca2974a936bc9b
中亚 <zhongya1024@163.com>
2025-03-03 22:55:10 +0800

    修改访问地址

--- vite.config.ts
+++ vite.config.ts
@@ -17,4 +17,10 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    host: true,  // 监听所有地址，包括 localhost 和 127.0.0.1
+    port: 5174,  // 使用备用端口
+    strictPort: true,  // 如果端口被占用，则中止
+    cors: true  // 启用 CORS
+  }
 })
